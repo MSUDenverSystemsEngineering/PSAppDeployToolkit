4d0e2d677d5add325254b63ad061791c2409e9ae
Steve Patterson <spatte30@msudenver.edu>
2018-05-16 15:43:10 -0600

    initial commit

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -61,15 +61,15 @@ Try {
 	##* VARIABLE DECLARATION
 	##*===============================================
 	## Variables: Application
-	[string]$appVendor = ''
+	[string]$appVendor = 'MSU Denver'
 	[string]$appName = ''
 	[string]$appVersion = ''
 	[string]$appArch = ''
 	[string]$appLang = 'EN'
 	[string]$appRevision = '01'
-	[string]$appScriptVersion = '3.7.0.1'
-	[string]$appScriptDate = '03/22/2018'
-	[string]$appScriptAuthor = '<author name>'
+	[string]$appScriptVersion = '1.0.0.0'
+	[string]$appScriptDate = '05/16/2018'
+	[string]$appScriptAuthor = 'Steve Patterson'
 	##*===============================================
 	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
 	[string]$installName = ''
@@ -137,6 +137,147 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
+		$PlainTextAccount="intake"
+		$PlainTextPassword=""
+
+		New-LocalUser -Name $PlainTextAccount -NoPassword -AccountNeverExpires -UserMayNotChangePassword | Set-LocalUser -PasswordNeverExpires $true
+		Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value 1
+		Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value $PlainTextAccount -ErrorAction SilentlyContinue
+		Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value $PlainTextPassword -ErrorAction SilentlyContinue
+		Execute-Process -Path “IE11-Setup-Branding.exe” -Parameters ‘/Q’
+
+		Dism /online /Enable-Feature /all /FeatureName:Client-EmbeddedShellLauncher
+
+		# Check if shell launcher license is enabled
+		function Check-ShellLauncherLicenseEnabled
+		{
+		    [string]$source = @"
+		using System;
+		using System.Runtime.InteropServices;
+
+		static class CheckShellLauncherLicense
+		{
+		    const int S_OK = 0;
+
+		    public static bool IsShellLauncherLicenseEnabled()
+		    {
+		        int enabled = 0;
+
+		        if (NativeMethods.SLGetWindowsInformationDWORD("EmbeddedFeature-ShellLauncher-Enabled", out enabled) != S_OK) {
+		            enabled = 0;
+		        }
+
+		        return (enabled != 0);
+		    }
+
+		    static class NativeMethods
+		    {
+		        [DllImport("Slc.dll")]
+		        internal static extern int SLGetWindowsInformationDWORD([MarshalAs(UnmanagedType.LPWStr)]string valueName, out int value);
+		    }
+
+		}
+"@
+		    $type = Add-Type -TypeDefinition $source -PassThru
+
+		    return $type[0]::IsShellLauncherLicenseEnabled()
+		}
+
+		[bool]$result = $false
+
+		$result = Check-ShellLauncherLicenseEnabled
+		"`nShell Launcher license enabled is set to " + $result
+		if (-not($result))
+		{
+		    "`nThis device doesn't have required license to use Shell Launcher"
+		    exit
+		}
+
+		$COMPUTER = "localhost"
+		$NAMESPACE = "root\standardcimv2\embedded"
+
+		# Create a handle to the class instance so we can call the static methods.
+		try {
+		    $ShellLauncherClass = [wmiclass]"\\$COMPUTER\${NAMESPACE}:WESL_UserSetting"
+		    } catch [Exception] {
+		    write-host $_.Exception.Message;
+		    write-host "Make sure Shell Launcher feature is enabled"
+		    exit
+		    }
+
+
+		# This well-known security identifier (SID) corresponds to the BUILTIN\Administrators group.
+
+		$Admins_SID = "S-1-5-32-544"
+
+		# Create a function to retrieve the SID for a user account on a machine.
+
+		function Get-UsernameSID($AccountName) {
+
+		    $NTUserObject = New-Object System.Security.Principal.NTAccount($AccountName)
+		    $NTUserSID = $NTUserObject.Translate([System.Security.Principal.SecurityIdentifier])
+
+		    return $NTUserSID.Value
+
+		}
+
+		# Get the SID for a user account named "Cashier". Rename "Cashier" to an existing account on your system to test this script.
+
+		$Cashier_SID = Get-UsernameSID($PlainTextAccount)
+
+		# Define actions to take when the shell program exits.
+
+		$restart_shell = 0
+		$restart_device = 1
+		$shutdown_device = 2
+
+		# Examples. You can change these examples to use the program that you want to use as the shell.
+
+
+		# This example sets the command prompt as the default shell, and restarts the device if the command prompt is closed.
+
+		    # $ShellLauncherClass.SetDefaultShell("cmd.exe", $restart_device)
+
+		# Display the default shell to verify that it was added correctly.
+
+		    # $DefaultShellObject = $ShellLauncherClass.GetDefaultShell()
+
+		    # "`nDefault Shell is set to " + $DefaultShellObject.Shell + " and the default action is set to " + $DefaultShellObject.defaultaction
+
+		# Set Internet Explorer as the shell for "Cashier", and restart the machine if Internet Explorer is closed.
+
+		    $ShellLauncherClass.SetCustomShell($Cashier_SID, "c:\program files\internet explorer\iexplore.exe -k https://ccparticipation.msudenver.edu/Titanium_Web/WCMenu.aspx", ($null), ($null), $restart_shell)
+
+		# Set Explorer as the shell for administrators.
+
+		    $ShellLauncherClass.SetCustomShell($Admins_SID, "explorer.exe")
+
+		# View all the custom shells defined.
+
+		    # "`nCurrent settings for custom shells:"
+		    # Get-WmiObject -namespace $NAMESPACE -computer $COMPUTER -class WESL_UserSetting | Select Sid, Shell, DefaultAction
+
+		# Enable Shell Launcher
+
+		     $ShellLauncherClass.SetEnabled($TRUE)
+
+		     $IsShellLauncherEnabled = $ShellLauncherClass.IsEnabled()
+
+
+		# Remove the new custom shells.
+
+		    # $ShellLauncherClass.RemoveCustomShell($Admins_SID)
+
+		    # $ShellLauncherClass.RemoveCustomShell($Cashier_SID)
+
+		# Disable Shell Launcher
+
+		    # $ShellLauncherClass.SetEnabled($FALSE)
+
+		    # $IsShellLauncherEnabled = $ShellLauncherClass.IsEnabled()
+
+		    # "`nEnabled is set to " + $IsShellLauncherEnabled.Enabled
+
 
 
 		##*===============================================
